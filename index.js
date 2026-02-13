7975b8ea40510355ef69d4020c616b8c0af89dca
favryadnan <favryadnan@gmail.com>
2026-02-13 15:37:16 +0100

    Update and rename aroma-log to index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Bienvenue sur Aroma Log</h1>
+      <p>Bienvenue dans notre boutique en ligne d'arômes et de senteurs ! Découvrez nos produits et investissements.</p>
+      <ul>
+        <li><a href="/products">Voir nos produits</a></li>
+        <li><a href="/investments">Voir les investissements disponibles</a></li>
+      </ul>
+    </div>
+  );
+}
