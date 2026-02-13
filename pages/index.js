1ab09256a4f997453a6ec92da3317971b437c30a
favryadnan <favryadnan@gmail.com>
2026-02-13 15:47:02 +0100

    Created pages folder and added index.js.

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Bienvenue sur Aroma Log</h1>
+      <p>Bienvenue dans notre boutique en ligne d'arômes et de senteurs ! Découvrez nos produits et investissements.</p>
+    </div>
+  );
+}
